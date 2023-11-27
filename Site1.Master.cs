ae55e7ba4c20c1ca9f76c8b10e7785de6e61e120
unknown <cssanchez@edu.upolitecnica.cr>
2023-11-27 17:11:01 -0600

    first commit

---
+++ Site1.Master.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExammenII
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LlenarGrid();
+            }
+        }
+         public void alertas(String texto)
+        {
+            string message = texto;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append("<script type = 'text/javascript'>");
+            sb.Append("window.onload=function(){");
+            sb.Append("alert('");
+            sb.Append(message);
+            sb.Append("')};");
+            sb.Append("</script>");
+            Page.ClientScript.RegisterClientScriptBlock(typeof(Type), "key", "script");
+            (Type, string, string) p = (this.GetType(), "alert", sb.ToString());
+
+        }
+
+        
+        protected void LlenarGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT *  FROM tipo"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            datagrid.DataSource = dt;
+                            datagrid.DataBind();  // actualizar el grid view
+                        }
+                    }
+                }
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            int resultado = clases.tipo.Agregar(tdescripcion.Text);
+
+            if (resultado > 0)
+            {
+                alertas("Tipo ingresado ha sido ingresado con exito");
+                tdescripcion.Text = string.Empty;
+                LlenarGrid();
+            }
+            else
+            {
+                alertas("Error al ingresar tipo");
+
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int resultado = clases.tipo.Borrar(int.Parse(tcodigo.Text));
+
+            if (resultado > 0)
+            {
+                alertas("Tipo ingresado ha sido ingresado con exito");
+                tdescripcion.Text = string.Empty;
+                LlenarGrid();
+            }
+            else
+            {
+                alertas("Error al ingresar tipo");
+
+            }
+
+
+        }
+
+
+
+       
+
+        protected void tcodigo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Bconsulta_Click(object sender, EventArgs e)
+        {
+            int codigo = int.Parse(tcodigo.Text);
+            string constr = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM USUARIO,EQUIPO,TECNICO"  ))
+
+
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        datagrid.DataSource = dt;
+                        datagrid.DataBind();  // actualizar el grid view
+                    }
+                }
+            }
+        }
+
+        protected void datagrid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
